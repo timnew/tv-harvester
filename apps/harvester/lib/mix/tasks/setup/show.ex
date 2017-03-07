4370e9de0f1e209bf1f8177497a12a830e5641bd
timnew <timnewcnyn@gmail.com>
2017-03-08 00:21:07 +1100

    Add setup.show task

---
+++ apps/harvester/lib/mix/tasks/setup/show.ex
@@ -0,0 +1,15 @@
+defmodule Mix.Tasks.Setup.Show do
+  use Mix.Task
+
+  import Show
+
+  @shortdoc "Setup Show"
+  def run(_) do
+    create_show("Flash", "http://www.dysfz.net/movie5156.html")
+    create_show("Legends of Tomorrow", "http://www.dysfz.net/movie8896.html")
+    create_show("Agents of SHIELD", "http://www.dysfz.net/movie4850.html")
+    create_show("Emerald City", "http://www.kmeiju.net/archives/4901.html")
+    create_show("Supergirl", "http://www.kmeiju.net/archives/4543.html")
+    create_show("Legion", "http://www.kmeiju.net/archives/4998.html")
+  end
+end
