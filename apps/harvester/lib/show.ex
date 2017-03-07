04507c06587dce9573cd8f8a848b8cdf9a99e2ed
timnew <timnewcnyn@gmail.com>
2017-03-08 00:03:59 +1100

    Add create_show and delete_all_shows

--- apps/harvester/lib/show.ex
+++ apps/harvester/lib/show.ex
@@ -13,4 +13,21 @@ defmodule Show do
     ConfigManager.keys([Show, "*"])
     |> Enum.map(&ConfigManager.get_struct(&1, Show))
   end
+
+  @spec create_show(String.t, String.test) :: :ok
+  def create_show(name, url) do
+    ConfigManager.put_hash([Show, name], [name: name, url: url])
+  end
+
+  @spec create_show(t) :: :ok
+  def create_show(%Show{name: name, url: url}) do
+    create_show(name, url)
+  end
+
+  @spec delete_all_shows :: non_neg_integer
+  def delete_all_shows do
+    [Show, "*"]
+    |> ConfigManager.keys()
+    |> ConfigManager.delete_all()
+  end
 end
--- apps/harvester/test/show_test.exs
+++ apps/harvester/test/show_test.exs
@@ -1,6 +1,8 @@
 defmodule ShowTest do
   use ExUnit.Case
 
+  import Show
+
   setup do
     ConfigManager.start_link("redis://locahost:6379/15")
 
@@ -10,21 +12,30 @@ defmodule ShowTest do
   end
 
   test "get_show" do
-    ConfigManager.command!(:hmset, ~w(Show:MyShow name MyShow url http://myshow.com))
+    create_show("MyShow", "http://myshow.com")
 
-    assert Show.get_show("MyShow") == %Show{name: "MyShow", url: "http://myshow.com"}
+    assert get_show("MyShow") == %Show{name: "MyShow", url: "http://myshow.com"}
   end
 
   test "get_all_shows" do
-    ConfigManager.command!(:hmset, ~w(Show:MyShow1 name MyShow1 url http://myshow.com))
-    ConfigManager.command!(:hmset, ~w(Show:MyShow2 name MyShow2 url http://myshow.com))
+    create_show(%Show{name: "MyShow1", url: "http://myshow.com"})
+    create_show(%Show{name: "MyShow2", url: "http://myshow.com"})
 
-    loaded_shows = Show.get_all_shows() |> Enum.sort()
+    loaded_shows = get_all_shows() |> Enum.sort()
     expected_shows = [
       %Show{name: "MyShow1", url: "http://myshow.com"},
       %Show{name: "MyShow2", url: "http://myshow.com"}
     ]
 
     assert loaded_shows == expected_shows
+  end
+
+  test "delete all shows" do
+    create_show(%Show{name: "MyShow1", url: "http://myshow.com"})
+    create_show(%Show{name: "MyShow2", url: "http://myshow.com"})
+
+    assert delete_all_shows() == 2
+
+    assert get_all_shows() == []
   end
 end
