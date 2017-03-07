0f8aeea88797ec419bc02a6dfd2dbdba2d5c7f7b
timnew <timnewcnyn@gmail.com>
2017-03-08 00:04:47 +1100

    Add setup project

---
+++ apps/setup/lib/setup.ex
@@ -0,0 +1,18 @@
+defmodule Setup do
+  @moduledoc """
+  Documentation for Setup.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Setup.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/setup/lib/setup/application.ex
@@ -0,0 +1,22 @@
+defmodule Setup.Application do
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Setup.Worker.start_link(arg1, arg2, arg3)
+      # worker(Setup.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Setup.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
