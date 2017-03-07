7095141052f72ede8f8c743f5012f9f96a3fc792
timnew <timnewcnyn@gmail.com>
2017-03-08 00:30:50 +1100

    Add mix task to setup shows

--- apps/harvester/lib/mix/tasks/setup/show.ex
+++ apps/harvester/lib/mix/tasks/setup/show.ex
@@ -5,6 +5,8 @@ defmodule Mix.Tasks.Setup.Show do
 
   @shortdoc "Setup Show"
   def run(_) do
+    Mix.Task.run "run"
+
     create_show("Flash", "http://www.dysfz.net/movie5156.html")
     create_show("Legends of Tomorrow", "http://www.dysfz.net/movie8896.html")
     create_show("Agents of SHIELD", "http://www.dysfz.net/movie4850.html")
