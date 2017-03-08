6365d1b8d51dd32db365f167a42cb8ce2dfb92a2
timnew <timnewcnyn@gmail.com>
2017-03-09 01:26:10 +1100

    Start process line

--- apps/harvester/lib/harvester.ex
+++ apps/harvester/lib/harvester.ex
@@ -21,13 +21,18 @@ defmodule Harvester do
 
   def start(_type, _args) do
     Logger.info("App start in executing mode...")
-    
+
     import Supervisor.Spec, warn: false
 
     HTTPoison.start
 
     children = [
-      worker(ConfigManager, [Application.get_env(:harvester, :redis)])
+      worker(ConfigManager, [Application.get_env(:harvester, :redis)]),
+      worker(Stage.ShowProvider, []),
+      worker(Stage.EpisodeExtractor, []),
+      worker(Stage.EpisodeExtractor, []),
+      worker(Stage.EpisodeBuffer, []),
+      worker(Stage.EpisodeProcessor, [])
     ]
 
     opts = [strategy: :one_for_one, name: Harvester.Supervisor]
