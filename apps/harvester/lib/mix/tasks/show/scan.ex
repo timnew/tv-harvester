35b2bf2c3a36e7b0a1456759e1cc028304015ba9
DD <timnew.wti@gmail.com>
2017-04-21 15:12:01 +1000

    Implment show.scan mix task

---
+++ apps/harvester/lib/mix/tasks/show/scan.ex
@@ -0,0 +1,19 @@
+defmodule Mix.Tasks.Show.Scan do
+  use Mix.Task
+
+  import Show
+  require PageData
+  require SiteParser
+  require Episode
+
+  @shortdoc "Scan Show"
+  def run(_) do
+    Mix.Task.run "run"
+
+    get_all_shows()
+    |> Stream.map(&PageData.fetch/1)
+    |> Stream.map(&SiteParser.parse/1)
+    |> Stream.flat_map(fn x -> x end)
+    |> Stream.each(&Episode.visit/1)
+  end
+end
